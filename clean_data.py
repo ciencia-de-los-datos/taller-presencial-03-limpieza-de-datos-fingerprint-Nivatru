50bbf530dd5395931f91232d2cc515281c7043e1
Nivatru <nvalenciat@unal.edu.co>
2024-03-08 18:58:26 -0500

    Update clean_data.py

--- clean_data.py
+++ clean_data.py
@@ -16,34 +16,27 @@ def create_fingerprint(df):
     df = df.copy()
 
     # 1. Copie la columna 'text' a la columna 'fingerprint'
-    df["key"] = df["text"]
-
     # 2. Remueva los espacios en blanco al principio y al final de la cadena
+    # 3. Convierta el texto a minúsculas
+    # 4. Transforme palabras que pueden (o no) contener guiones por su version sin guion.
+    # 5. Remueva puntuación y caracteres de control
+    # 6. Convierta el texto a una lista de tokens
+    # 7. Transforme cada palabra con un stemmer de Porter
+    # 8. Ordene la lista de tokens y remueve duplicados
+    # 9. Convierta la lista de tokens a una cadena de texto separada por espacios
+
+    df["key"] = df["text"]
     df["key"] = df["key"].str.rstrip()
     df["key"] = df["key"].str.lstrip()
-
-    # 3. Convierta el texto a minúsculas
     df["key"] = df["key"].str.lower()
-
-    # 4. Transforme palabras que pueden (o no) contener guiones por su version sin guion.
     df["key"] = df["key"].str.replace("-","")
-
-    # 5. Remueva puntuación y caracteres de control
     df["key"] = df["key"].str.translate(
         str.maketrans("", "", "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~")
     )
-
-    # 6. Convierta el texto a una lista de tokens
     df["key"] = df["key"].str.split()
-
-    # 7. Transforme cada palabra con un stemmer de Porter
     stemer = nltk.PorterStemmer()
     df["key"] = df["key"].apply(lambda x: [stemer.stem(word) for word in x])
-
-    # 8. Ordene la lista de tokens y remueve duplicados
     df["key"] = df["key"].apply(lambda x: sorted(set(x)))
-
-    # 9. Convierta la lista de tokens a una cadena de texto separada por espacios
     df["key"] = df["key"].str.join(" ")
 
     return df
@@ -55,15 +48,13 @@ def generate_cleaned_column(df):
     df = df.copy()
 
     # 1. Ordene el dataframe por 'fingerprint' y 'text'
-    df = df.sort_values(by=["key", "text"], ascending=[True, True])
-
     # 2. Seleccione la primera fila de cada grupo de 'fingerprint'
-    keys = df.drop_duplicates(subset="key", keep="first")
-
     # 3.  Cree un diccionario con 'fingerprint' como clave y 'text' como valor
-    key_dict = dict(zip(keys["key"], keys["text"]))
-
     # 4. Cree la columna 'cleaned' usando el diccionario
+    
+    df = df.sort_values(by=["key", "text"], ascending=[True, True])
+    keys = df.drop_duplicates(subset="key", keep="first")
+    key_dict = dict(zip(keys["key"], keys["text"]))
     df["cleaned"] = df["key"].map(key_dict)
 
     return df
